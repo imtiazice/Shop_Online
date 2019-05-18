006797e456cc00905f01c1cda09f85168ee98e97
imtiazice <mahrabimtiaz@gmail.com>
2019-05-18 20:51:46 +0600

    Shop_Online

---
+++ app/src/main/java/com/imtiaz/e_commerce_app/Interface/ItemClickListner.java
@@ -0,0 +1,8 @@
+package com.imtiaz.e_commerce_app.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner {
+    void onClick(View view, int position, boolean isLongClick);
+
+}
---
+++ app/src/main/java/com/imtiaz/e_commerce_app/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.imtiaz.e_commerce_app.Prevalent;
+
+import com.imtiaz.e_commerce_app.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
